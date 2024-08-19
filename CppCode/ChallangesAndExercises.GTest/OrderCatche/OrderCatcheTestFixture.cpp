83cb6beb9981e2250d1b43c77631ab32bd35141c
Noxam Sherei <noxamsherei@gmail.com>
2024-08-19 15:05:30 +0200

    Include Chalanges and exercises repository

---
+++ CppCode/ChallangesAndExercises.GTest/OrderCatche/OrderCatcheTestFixture.cpp
@@ -0,0 +1,199 @@
+#pragma once
+#include "..\pch.h"
+#include <OrderCatche\OrderCacheImpl.h>
+#include <unordered_map>
+#include <algorithm>
+
+class OrderCatcheTestFixture : public ::testing::Test {
+public:
+	void SetUp() {
+		orderCache = std::make_shared<OrderCacheImpl>();
+	}
+	void TearDown() {
+		vec_orders.clear();
+		orderCache.reset();
+	}
+	std::vector<Order*> vec_orders;
+	std::shared_ptr<OrderCacheInterface> orderCache;
+};
+
+TEST_F(OrderCatcheTestFixture, Example1) {
+	vec_orders = {
+			new Order("OrdId1", "SecId1", "Buy", 1000, "User1", "CompanyA"),
+			new Order("OrdId2", "SecId2", "Sell", 3000, "User2", "CompanyB"),
+			new Order("OrdId3", "SecId1", "Sell", 500, "User3", "CompanyA"),
+			new Order("OrdId4", "SecId2", "Buy", 600, "User4", "CompanyC"),
+			new Order("OrdId5", "SecId2", "Buy", 100, "User5", "CompanyB"),
+			new Order("OrdId6", "SecId3", "Buy", 1000, "User6", "CompanyD"),
+			new Order("OrdId7", "SecId2", "Buy", 2000, "User7", "CompanyE"),
+			new Order("OrdId8", "SecId2", "Sell", 5000, "User8", "CompanyE")
+	};
+	for (auto elem : vec_orders) {
+		orderCache->addOrder(*elem);
+	}
+	auto expectedMatch1 = 0;
+	auto expectedMatch2 = 2700;
+	auto expectedMatch3 = 0;
+	EXPECT_EQ(orderCache->getMatchingSizeForSecurity("SecId1"), expectedMatch1);
+	EXPECT_EQ(orderCache->getMatchingSizeForSecurity("SecId2"), expectedMatch2);
+	ASSERT_EQ(orderCache->getMatchingSizeForSecurity("SecId3"), expectedMatch3);
+}
+TEST_F(OrderCatcheTestFixture, Example2) {
+	vec_orders = {
+		new Order("OrdId1", "SecId1", "Sell", 100, "User10", "Company2"),
+		new Order("OrdId2", "SecId3", "Sell", 200, "User8", "Company2"),
+		new Order("OrdId3", "SecId1", "Buy", 300, "User13", "Company2"),
+		new Order("OrdId4", "SecId2", "Sell", 400, "User12", "Company2"),
+		new Order("OrdId5", "SecId3", "Sell", 500, "User7", "Company2"),
+		new Order("OrdId6", "SecId3", "Buy", 600, "User3", "Company1"),
+		new Order("OrdId7", "SecId1", "Sell", 700, "User10", "Company2"),
+		new Order("OrdId8", "SecId1", "Sell", 800, "User2", "Company1"),
+		new Order("OrdId9", "SecId2", "Buy", 900, "User6", "Company2"),
+		new Order("OrdId10", "SecId2", "Sell", 1000, "User5", "Company1"),
+		new Order("OrdId11", "SecId1", "Sell", 1100, "User12", "Company2"),
+		new Order("OrdId12", "SecId2", "Buy", 1200, "User9", "Company2"),
+		new Order("OrdId13", "SecId1", "Sell", 1300, "User1", "Company2"),
+	};
+	for (auto elem : vec_orders) {
+		orderCache->addOrder(*elem);
+	}
+	auto expectedMatch1 = 300;
+	auto expectedMatch2 = 1000;
+	auto expectedMatch3 = 600;
+	EXPECT_EQ(orderCache->getMatchingSizeForSecurity("SecId1"), expectedMatch1);
+	EXPECT_EQ(orderCache->getMatchingSizeForSecurity("SecId2"), expectedMatch2);
+	ASSERT_EQ(orderCache->getMatchingSizeForSecurity("SecId3"), expectedMatch3);
+}
+TEST_F(OrderCatcheTestFixture, Example3) {
+	vec_orders = {
+		new Order("OrdId1", "SecId3", "Sell", 100, "User1", "Company1"),
+		new Order("OrdId2", "SecId3", "Sell", 200, "User3", "Company2"),
+		new Order("OrdId3", "SecId1", "Buy", 300, "User2", "Company1"),
+		new Order("OrdId4", "SecId3", "Sell", 400, "User5", "Company2"),
+		new Order("OrdId5", "SecId2", "Sell", 500, "User2", "Company1"),
+		new Order("OrdId6", "SecId2", "Buy", 600, "User3", "Company2"),
+		new Order("OrdId7", "SecId2", "Sell", 700, "User1", "Company1"),
+		new Order("OrdId8", "SecId1", "Sell", 800, "User2", "Company1"),
+		new Order("OrdId9", "SecId1", "Buy", 900, "User5", "Company2"),
+		new Order("OrdId10", "SecId1", "Sell", 1000, "User1", "Company1"),
+		new Order("OrdId11", "SecId2", "Sell", 1100, "User16", "Company2"),
+	};
+	for (auto elem : vec_orders) {
+		orderCache->addOrder(*elem);
+	}
+	auto expectedMatch1 = 900;
+	auto expectedMatch2 = 600;
+	auto expectedMatch3 = 0;
+	EXPECT_EQ(orderCache->getMatchingSizeForSecurity("SecId1"), expectedMatch1);
+	EXPECT_EQ(orderCache->getMatchingSizeForSecurity("SecId2"), expectedMatch2);
+	ASSERT_EQ(orderCache->getMatchingSizeForSecurity("SecId3"), expectedMatch3);
+}
+
+TEST_F(OrderCatcheTestFixture, CancelOrdersByOrderId) {
+	vec_orders = {
+		new Order("OrdId1", "SecId1", "Sell", 100, "User10", "Company2"),
+		new Order("OrdId2", "SecId3", "Sell", 200, "User8", "Company2"),
+		new Order("OrdId3", "SecId1", "Buy", 300, "User13", "Company2"),
+	};
+	for (auto elem : vec_orders) {
+		orderCache->addOrder(*elem);
+	}
+	{
+		orderCache->cancelOrder("OrdId1");
+		std::vector<Order> orders1 = orderCache->getAllOrders();
+		bool x = std::any_of(orders1.begin(), orders1.end(),
+			[](const Order& elem) {return elem.orderId() == "OrdId1"; });
+		ASSERT_FALSE(x);
+	}
+	{
+		orderCache->cancelOrder("OrdId3");
+		std::vector<Order> orders1 = orderCache->getAllOrders();
+		bool x = std::any_of(orders1.begin(), orders1.end(),
+			[](const Order& elem) {return elem.orderId() == "OrdId3"; });
+		ASSERT_FALSE(x);
+	}
+	ASSERT_TRUE(dynamic_cast<OrderCacheImpl*>(orderCache.get())->noEmptySecurityAndUser());
+}
+TEST_F(OrderCatcheTestFixture, CancelOrdersBySecMin) {
+	vec_orders = {
+		new Order("OrdId1", "SecId1", "Sell", 100, "User10", "Company2"),
+		new Order("OrdId2", "SecId3", "Sell", 200, "User8", "Company2"),
+		new Order("OrdId3", "SecId1", "Buy", 300, "User13", "Company2"),
+		new Order("OrdId4", "SecId2", "Sell", 400, "User12", "Company2"),
+		new Order("OrdId5", "SecId3", "Sell", 500, "User7", "Company2"),
+		new Order("OrdId6", "SecId3", "Buy", 600, "User3", "Company1"),
+	};
+	for (auto elem : vec_orders) {
+		orderCache->addOrder(*elem);
+	}
+	orderCache->cancelOrdersForSecIdWithMinimumQty("SecId3", 300);
+	std::vector<Order> orders1 = orderCache->getAllOrders();
+	for (const auto& order : orders1) {
+		if (order.securityId() != "SecId3") {
+			continue;
+		}
+		if (order.qty() < 300) {
+			continue;
+		}
+		ASSERT_FALSE(true);
+	}
+	ASSERT_TRUE(dynamic_cast<OrderCacheImpl*>(orderCache.get())->noEmptySecurityAndUser());
+}
+
+TEST_F(OrderCatcheTestFixture, CancelOrdersByUser) {
+	vec_orders.clear();
+	vec_orders.push_back(new Order("OrdId1", "SecId2", "Sell", 100, "User1", "Company2"));
+	vec_orders.push_back(new Order("OrdId2", "SecId1", "Sell", 100, "User2", "Company2"));
+	vec_orders.push_back(new Order("OrdId3", "SecId1", "Sell", 100, "User3", "Company2"));
+
+	for (auto elem : vec_orders) {
+		orderCache->addOrder(*elem);
+	}
+	{
+		orderCache->cancelOrdersForUser("User2");
+		std::vector<Order> orders1 = orderCache->getAllOrders();
+		bool x = std::any_of(orders1.begin(), orders1.end(),
+			[](const Order& elem) {return elem.user() == "User2"; });
+		ASSERT_FALSE(x);
+	}
+	{
+		orderCache->cancelOrdersForUser("User3");
+		std::vector<Order> orders1 = orderCache->getAllOrders();
+		bool x = std::any_of(orders1.begin(), orders1.end(),
+			[](const Order& elem) {return elem.user() == "User3"; });
+		ASSERT_FALSE(x);
+	}
+	ASSERT_TRUE(dynamic_cast<OrderCacheImpl*>(orderCache.get())->noEmptySecurityAndUser());
+}
+
+TEST_F(OrderCatcheTestFixture, ThreadSafe) {
+	for (size_t i = 0; i < 500; i++)
+	{
+		std::string id = "OrdId" + std::to_string(i);
+		std::string SecId = "SecId" + std::to_string((i % 4));
+		std::string sell = i % 3 == 0 ? "Sell" : "Buy";
+		std::string user = "User" + std::to_string((i % 7));
+		std::string Company = "Company" + std::to_string((i % 7));
+		vec_orders.push_back(
+			new Order(id, SecId, sell, 1000 * ((i % 3) + 1), user, Company));
+	}
+	for (size_t i = 0; i < 250; i++)
+	{
+		orderCache->addOrder(*vec_orders[i]);
+	}
+	std::list<std::thread> threads;
+
+	for (size_t i = 250; i < vec_orders.size() - 50; i++)
+	{
+		threads.push_back(std::thread([&]() {orderCache->addOrder(*vec_orders[i]); }));
+	};
+	for (size_t i = 0; i < 3; i++)
+	{
+		threads.push_back(std::thread([&]() {orderCache->getMatchingSizeForSecurity((*vec_orders[i]).securityId()); }));
+
+	};
+	for (auto& th : threads)
+	{
+		th.join();
+	}
+}
---
+++ CppCode/ChallengesAndExercises/src/OrderCatche/OrderCacheImpl.cpp
@@ -0,0 +1,201 @@
+#pragma once
+#include "OrderCacheImpl.h"
+#include <iterator>
+
+
+/// <summary>
+/// Checks if there are no securities or users with empty orders.
+/// Function mainly for testing
+/// </summary>
+const bool OrderCacheImpl::noEmptySecurityAndUser() const {
+	for (const auto& sec : m_securitiesOrders_buySell) {
+		if (sec.second.first.empty() && sec.second.second.empty())
+			return false;
+	}
+	for (const auto& sec : m_usersOrders) {
+		if (sec.second.empty())
+			return false;
+	}
+	return true;
+}
+
+// Checks if an order does not exist in the order cache by its ID.
+inline const bool OrderCacheImpl::orderNotExist(const std::string& orderId) const
+{
+	return m_ordersByOrderId.find(orderId) == m_ordersByOrderId.end();
+}
+
+// Checks if an order has the correct format (i.e., quantity is not zero and side is either "Buy" or "Sell").
+inline const bool OrderCacheImpl::orderHasCorrectFormat(const Order& order) const
+{
+	return order.qty() == 0 || !(order.side() == "Buy" || order.side() == "Sell");
+}
+
+// Checks if a user does not exist in the user orders map by their user name.
+inline const bool OrderCacheImpl::userNotExist(const std::string& userName) const
+{
+	return m_usersOrders.find(userName) == m_usersOrders.end();
+}
+
+// Checks if a security ID does not exist in the securities orders map.
+inline const bool OrderCacheImpl::securityIDNotExist(const std::string& securityId) const
+{
+	return m_securitiesOrders_buySell.find(securityId) == m_securitiesOrders_buySell.end();
+}
+
+
+void OrderCacheImpl::addOrder(Order order) {
+	const std::string& orderId = order.orderId();
+	const std::string& securityId = order.securityId();
+	const std::string& userId = order.user();
+	const bool isBuy = order.side() == "Buy";
+	std::lock_guard<std::mutex> guard(m_mutex);
+
+	// If the order already exists or has an incorrect format, do nothing.
+	if (!orderNotExist(orderId)) {
+		return;
+	}
+
+	if (orderHasCorrectFormat(order)) {
+		return;
+	}
+
+	// Add the order to the orders map.
+	m_ordersByOrderId.insert({ orderId,std::make_shared<Order>(std::move(order)) });
+	// Associate the order with the user.
+	m_usersOrders[userId][orderId] = m_ordersByOrderId[orderId];
+	// Store the order in the appropriate buy or sell map based on its side.
+	if (isBuy) {
+		m_securitiesOrders_buySell[securityId].first[orderId] = m_ordersByOrderId[orderId];
+	}
+	else {
+		m_securitiesOrders_buySell[securityId].second[orderId] = m_ordersByOrderId[orderId];
+	}
+}
+
+void OrderCacheImpl::cancelOrder(const std::string& orderId) {
+	std::lock_guard<std::mutex> guard(m_mutex);
+	// If the order doesn't exist, do nothing.
+	if (orderNotExist(orderId)) {
+		return;
+	}
+	// Remove the order from the cache.
+	removeTargetedFromCashe(orderId);
+}
+
+void OrderCacheImpl::cancelOrdersForUser(const std::string& user) {
+	std::lock_guard<std::mutex> guard(m_mutex);
+	if (userNotExist(user)) {
+		return;
+	}
+	std::vector<std::string> ordersToRemove;
+	// Collect all order IDs from the user and add them to the ordersToRemove vector.
+	std::transform(m_usersOrders[user].begin(), m_usersOrders[user].end(), std::back_inserter(ordersToRemove),
+		[](const std::pair<std::string, OrderPtr> pair) { return pair.first; });
+
+	// Remove the targeted orders from the cache.
+	removeTargetedFromCashe(ordersToRemove);
+}
+
+void OrderCacheImpl::cancelOrdersForSecIdWithMinimumQty(const std::string& securityId, unsigned int minQty) {
+	std::lock_guard<std::mutex> guard(m_mutex);
+	// If the security ID doesn't exist, do nothing.
+	if (securityIDNotExist(securityId)) {
+		return;
+	}
+
+	std::vector<std::string> ordersToRemove;
+		
+	// Collect orders from the buy side that meet the minimum quantity requirement.
+	for (const auto& [orderId, orders] : m_securitiesOrders_buySell[securityId].first) {
+		if (orders->qty() >= minQty) {
+			ordersToRemove.push_back(orders->orderId());
+		}
+	}
+	// Collect orders from the sell side that meet the minimum quantity requirement.
+	for (const auto& [orderId, orders] : m_securitiesOrders_buySell[securityId].second) {
+		if (orders->qty() >= minQty) {
+			ordersToRemove.push_back(orders->orderId());
+		}
+	}
+	// Remove the targeted orders from the cache.
+	removeTargetedFromCashe(ordersToRemove);
+}
+
+unsigned int OrderCacheImpl::getMatchingSizeForSecurity(const std::string& securityId) {
+	std::lock_guard<std::mutex> guard(m_mutex);
+	// If the security ID doesn't exist, return 0.
+	if (securityIDNotExist(securityId)) {
+		return 0;
+	}
+	const auto& [buys, sells] = m_securitiesOrders_buySell[securityId];
+
+	std::unordered_map<std::string, unsigned int> buyOrders;
+	std::unordered_map<std::string, unsigned int> sellOrders;
+
+	// Accumulate the quantities for buy orders by company.
+	for (const auto& [id, order] : buys) {
+		buyOrders[order->company()] += order->qty();
+	}
+	// Accumulate the quantities for sell orders by company.
+	for (const auto& [id, order] : sells) {
+		sellOrders[order->company()] += order->qty();
+	}
+
+	unsigned int totalMatch = 0;
+
+	// Calculate the total matching size by comparing buy and sell orders.
+	// Worst-case scenario is O(n/2).
+	for (auto& [buyCompany, buyValue] : buyOrders) {
+		for (auto& [sellCompany, sellValue] : sellOrders) {
+			if (buyCompany == sellCompany) {
+				continue;
+			}
+			unsigned int matchQty = std::min(buyValue, sellValue);
+			totalMatch += matchQty;
+			buyValue -= matchQty;
+			sellValue -= matchQty;
+		}
+	}
+	return totalMatch;
+}
+
+std::vector<Order> OrderCacheImpl::getAllOrders() const {
+	std::lock_guard<std::mutex> guard(m_mutex);
+	std::vector<Order> allOrders;
+	// Copy all orders from the cache to the allOrders vector.
+	for (const auto& pair : m_ordersByOrderId) {
+		allOrders.push_back(*pair.second);
+	}
+	return allOrders;
+}
+
+/// <summary>
+/// Removes a single order from the cache by its order ID.
+/// </summary>
+/// <param name="orderId">The ID of the order to remove.</param>
+void OrderCacheImpl::removeOrderFromCashe(const std::string& orderId) {
+	const Order& order = *(m_ordersByOrderId[orderId].get());
+	const std::string& securityId = order.securityId();
+	const std::string& userId = order.user();
+
+	// Remove the order from the user's order map.
+	// If the user has no more orders, remove the user from the map.
+	m_usersOrders[userId].erase(orderId);
+	if (m_usersOrders[userId].empty()) {
+		m_usersOrders.erase(userId);
+	}
+
+	// Remove the order from the security's buy or sell map.
+	// If the security has no more buy or sell orders, remove the security from the map.
+	m_securitiesOrders_buySell[securityId].first.erase(orderId);
+	m_securitiesOrders_buySell[securityId].second.erase(orderId);
+	if (m_securitiesOrders_buySell[securityId].first.empty()) {
+		if (m_securitiesOrders_buySell[securityId].second.empty()) {
+			m_securitiesOrders_buySell.erase(securityId);
+		}
+	}
+
+	// Finally, remove the order from the orders map.
+	m_ordersByOrderId.erase(orderId);
+}
